ad5cd01a7f7ddaca7be4c5da01608dad6e3b8b37
AkshayPatwardhan <qwerty.akshay@gmail.com>
2017-04-07 12:29:27 +0100

    Update uart.h

--- uart.h
+++ uart.h
@@ -11,11 +11,16 @@
 #include <sys/types.h>
 #include <string.h>
 
+#define UART_FUNCTION_SUCCESSFUL        0
+#define UART_NUMBER_INCORRECT           1
+#define UART_BAUDRATE_INCORRECT         2
+#define UART_FIFO_ERROR                 3
 
-int uartInitialize(int uartNumber, int baudRate);
-int uartWriteByte(int uartNumber, unsigned char uwb);
-int uartWriteLine(int uartNumber, char uwl[]);
+unsigned char uartInitialize(int uartNumber, int baudRate);
+unsigned char uartWriteByte(int uartNumber, unsigned char uwb);
+unsigned char uartWriteLine(int uartNumber, unsigned char uwl[]);
 unsigned char uartRead(int uartNumber);
-int uartDeinit(int uartNumber);
+unsigned char uartWaitTillTxComplete(int uartNumber);
+unsigned char uartDeinit(int uartNumber);
 
 #endif // UART_H
